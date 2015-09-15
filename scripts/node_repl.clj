b678946de3ff3863c1c1973bdeb542fb1e42038e
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-09-15 13:08:11 -0500

    Add scripts for working with node.js.
    Create scripts for invoking the JavaScript file generated by
    ClojureScript in the node.js environment. In addition, create a script
    for creating a node.js REPL in that same environment.
    
    This scripts are combinations of the files used to build the application
    and to create a REPL and the scripts available from the ClojureScript
    Quick Start (../cljs_qs) project.

---
+++ scripts/node_repl.clj
@@ -0,0 +1,15 @@
+(require 'cljs.repl)
+(require 'cljs.build.api)
+(require 'cljs.repl.node)
+
+;; Compile all scripts in the "src" directory and write the result to "out/main.js."
+(cljs.build.api/build "src"
+                      {:main 'time-calc.core
+                       :output-to "out/time_calc.js"
+                       :output-dir "out"
+                       :verbose true})
+
+;; Invoke the node.js REPL.
+(cljs.repl/repl (cljs.repl.node/repl-env)
+                :watch "src"
+                :output-dir "out")
