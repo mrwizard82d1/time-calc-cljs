23635b75fdc54abf7696dd5566ef4c2194c91dda
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-09-16 23:00:47 -0500

    Changed function to read from the file system.

--- src/time_calc/core.cljs
+++ src/time_calc/core.cljs
@@ -1,10 +1,17 @@
 (ns time-calc.core
   (:require [cljs.nodejs :as nodejs]))
 
+(def fs (js/require "fs"))
+
 (nodejs/enable-util-print!)
 
 (defn -main [& args]
-  (println "Hello, Node.js World!"))
+  (.readFile fs "time.txt" (fn [err data]
+                             (if err
+                               (throw err)
+                               (println (str data)))))
+  ;; Note that this next line executes FIRST.
+  (println "Hello, Node.js World! (Surprisingly prints first.)"))
 
 (set! *main-cli-fn* -main)
 
