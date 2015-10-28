bd3829a7b6826c09cc1496019446194d44b6645d
Lawrence Allan Jones <lawrence.jones@openlink.com>
2015-10-28 13:35:39 -0500

    Repair error: node_repl must target node.js.

--- scripts/node_repl.clj
+++ scripts/node_repl.clj
@@ -7,6 +7,7 @@
                       {:main 'time-calc.core
                        :output-to "out/time_calc.js"
                        :output-dir "out"
+                       :target :nodejs
                        :verbose true})
 
 ;; Invoke the node.js REPL.
